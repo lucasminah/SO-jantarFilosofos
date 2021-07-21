a4240a17e15abec0ac831cb641269cafdcdcc31a
Lucas Miná <lucas.mina@ccc.ufcg.edu.br>
2021-07-21 18:29:50 -0300

    Monitor: Separando dados úteis em variáveis
    Facilitando o teste com valores diferentes.

--- monitor/Main.java
+++ monitor/Main.java
@@ -1,19 +1,21 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        Filosofo[] filosofos = new Filosofo[6];
-        Object[] talheres = new Object[6];
+        int numeroFilosofos = 6;
+        int tempoEspera = 500;
+        Filosofo[] filosofos = new Filosofo[numeroFilosofos];
+        Object[] talheres = new Object[numeroFilosofos];
 
         for (int i = 0; i < 6; i++) {
             talheres[i] = new Object();
         }
 
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < numeroFilosofos; i++) {
             Object talherEsquerda = talheres[i];
-            Object talherDireita = talheres[(i + 1) % 6];
-            if (i == 5) {
-                filosofos[i] = new Filosofo(talherEsquerda, talherDireita, 500);
+            Object talherDireita = talheres[(i + 1) % numeroFilosofos];
+            if (i == numeroFilosofos - 1) {
+                filosofos[i] = new Filosofo(talherEsquerda, talherDireita, tempoEspera);
             } else {
-                filosofos[i] = new Filosofo(talherDireita, talherEsquerda, 500);
+                filosofos[i] = new Filosofo(talherDireita, talherEsquerda, tempoEspera);
             }
             Thread t = new Thread(filosofos[i], "Filosofo " + (i + 1));
             t.start();
